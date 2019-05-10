6a6a77a9d144d01ddc82280d1598668c38824ea4
shiram <shirambereh@gmail.com>
2019-05-10 17:28:52 +0300

    First Commit
    Data Structures

---
+++ data_structures/stack/check_balanced_paren.py
@@ -0,0 +1,60 @@
+from stack import Stack
+
+def is_match(p1, p2):
+    if p1 == "(" and p2 == ")":
+        return True
+    elif p1 == "[" and p2 == "]":
+        return True
+    elif p1 == "{" and p2 == "}":
+        return True
+    else:
+        return False
+
+# Implemetation Using a Stack 
+def is_paren_balanced_stack(paren_string):
+    s = Stack()
+    is_balanced = True
+    index = 0
+
+    while index < len(paren_string) and is_balanced:
+        paren = paren_string[index]
+        if paren in "({[":
+            s.push(paren)
+        else:
+            if s.is_empty():
+                is_balanced = False
+            else:
+                top = s.pop()
+                if not is_match(top, paren):
+                    is_balanced = False
+        index += 1
+    if s.is_empty() and is_balanced:
+        return True
+    else:
+        return False
+
+# Implemetation Using a List
+def is_paren_balanced_list(paren_string):
+    s = []
+    is_balanced = True
+    index = 0
+
+    while index < len(paren_string) and is_balanced:
+        paren = paren_string[index]
+        if paren in "({[":
+            s.append(paren)
+        else:
+            if len(s) == 0:
+                is_balanced = False
+            else:
+                top = s.pop()
+                if not is_match(top, paren):
+                    is_balanced = False
+        index += 1
+    if len(s) == 0 and is_balanced:
+        return True
+    else:
+        return False
+
+# print(is_paren_balanced_stack("{{({})}}"))
+print(is_paren_balanced_list("{{({})}}"))
